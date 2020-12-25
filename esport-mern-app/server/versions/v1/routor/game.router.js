536f2ae6e0bc73318b2a7e13f3b82ba5abf80dba
Vikrant <vikrant@chalkstreet.com>
2020-12-25 18:15:26 +0530

    Create public and private game api

--- esport-mern-app/server/versions/v1/routor/game.router.js
+++ esport-mern-app/server/versions/v1/routor/game.router.js
@@ -1,8 +1,10 @@
 const { request } = require("express");
 const express = require("express");
 const router = express.Router();
-const { getGames } = require("../services/game.service");
+const { getGames, createGame } = require("../services/game.service");
+const token = require("../../../utils/jwtVerifier")
 
 router.get("/", getGames);
+router.post("/", token.verifyToken, createGame);
 
 module.exports = router;
