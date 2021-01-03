f0ce1c6ccc4b59a0bd99528aa15867a12c4d8173
Vikrant <vikrant@chalkstreet.com>
2021-01-03 22:45:48 +0530

    mail gun api integeration

--- esport-mern-app/server/utils/common.util.js
+++ esport-mern-app/server/utils/common.util.js
@@ -1,9 +1,11 @@
+const keys = require("../config/keys");
 const crypto = require('crypto'),
   algorithm = 'aes-256-ctr',
   password = 'd6F3Efeq';
 const jwtSecret = "VG"
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
+const { title } = require("process");
 const encrypt = (text) => {
   let cipher = crypto.createCipher(algorithm, password)
   let crypted = cipher.update(text, 'utf8', 'hex')
@@ -25,4 +27,20 @@ const generateToken = (data) => {
   return { token: token, 'jid': data['jid'] };
 }
 
-module.exports = { decrypt, encrypt, generateToken };
+const send_mail = (mail, subject, title, body, btn_link, btn_name) => {
+  const mailgun = require("mailgun-js");
+  const DOMAIN = keys.mailgun.domain;
+  const mg = mailgun({ apiKey: keys.mailgun.apiKey, domain: DOMAIN });
+  const data = {
+    from: "Vikrant <postmaster@" + keys.mailgun.domain + ">",
+    to: mail,
+    subject: subject,
+    template: "esport-action",
+    'h:X-Mailgun-Variables': JSON.stringify({ "title": title, "body": body, "btn_link": btn_link, "btn_name": btn_name })
+  };
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+}
+
+module.exports = { decrypt, encrypt, generateToken, send_mail };
