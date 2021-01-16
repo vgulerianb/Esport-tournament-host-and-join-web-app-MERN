d84a5708904faa9172e6200d192739903f781c5c
Vikrant <vikrant@chalkstreet.com>
2021-01-16 19:09:20 +0530

    b

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const cors = require("cors");
 
 const APP_URL = path.resolve(__dirname, "../client/build");
 const APP_URL_HOME = path.resolve(__dirname, "../client/build/index.html");
-let dir = __dirname.replace("/server","/client/");
+let dir = __dirname.replace("/server", "/client/");
 // compress all responses
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 app.use(cors());
 app.use("/api/", versionRouter);
@@ -24,5 +24,6 @@ app.get("*", async (req, res) => {
 //all routes
 
 app.listen(PORT, () => {
+    console.log(process.env.NODE_ENV);
     console.log("Login service is listening at PORT ", PORT);
 });
