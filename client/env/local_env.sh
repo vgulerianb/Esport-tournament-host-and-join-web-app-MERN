ebbba009cd67482d909d0d88c7a4d531da2778a6
Vikrant <vikrant@chalkstreet.com>
2021-01-16 18:01:33 +0530

    DO NOT MERGE

--- client/env/local_env.sh
+++
@@ -1,2 +0,0 @@
-export REACT_APP_G_HOME_URL=http://localhost:5000/
-export PORT=5000
--- client/src/constants/constants.js
+++
@@ -1,2 +0,0 @@
-export const G_HOME_URL = process.env.REACT_APP_G_HOME_URL;
-export const G_API_URL = process.env.REACT_APP_G_API_URL;
