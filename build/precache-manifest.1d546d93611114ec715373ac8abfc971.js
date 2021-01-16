0066b88a05b5540a2a5aea8982501b0a8d2fee9c
Vikrant <vikrant@chalkstreet.com>
2021-01-16 19:48:24 +0530

    Do not merge

---
+++ build/precache-manifest.1d546d93611114ec715373ac8abfc971.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2da496a44518348ffb03e586893203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa58f937921dea82fed",
+    "url": "/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "cd15c785da0dc3a5f314",
+    "url": "/static/css/main.bf01635a.chunk.css"
+  },
+  {
+    "revision": "5aa58f937921dea82fed",
+    "url": "/static/js/2.598784fc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.598784fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd15c785da0dc3a5f314",
+    "url": "/static/js/main.536d295e.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "d64adc8a1d6a67800200d740ea6d8faa",
+    "url": "/static/media/404.d64adc8a.png"
+  }
+]);
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require("path");
 const versionRouter = require("./versions/v1/version.router");
 const cors = require("cors");
 
-const APP_URL = path.resolve(__dirname, "../client/build");
-const APP_URL_HOME = path.resolve(__dirname, "../client/build/index.html");
+const APP_URL = path.resolve(__dirname, "./build");
+const APP_URL_HOME = path.resolve(__dirname, "./build/index.html");
 let dir = __dirname.replace("/server", "/client/");
 // compress all responses
 app.use(express.json());
