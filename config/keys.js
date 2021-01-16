055c42f716fb0cd2c1fc23a1ecc88856a8eac3eb
Vikrant <vikrant@chalkstreet.com>
2021-01-16 18:15:54 +0530

    b

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mongodb: {
+        dev: {
+            host: "",
+            port: "",
+            database: "",
+            username: "",
+            password: "",
+            host_1: "",
+            port_1: "",
+            host_2: "",
+            port_2: "",
+            replicaSet: ""
+        },
+    },
+    mailgun: {
+        apiKey: "",
+        domain: ""
+    }
+};
