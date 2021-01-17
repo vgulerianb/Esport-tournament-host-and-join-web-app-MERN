81d23435cc0a9b138070d58f691132f16b630ff4
Vikrant <vikrant@chalkstreet.com>
2021-01-17 18:38:32 +0530

    Do not merge

---
+++ build/precache-manifest.5ed0b1891d094eabffb4c32710b96dcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7403a9df93c3a0622760aa03b0c006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ed86766552d0823811",
+    "url": "/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "4a297bb9ce13396a6e08",
+    "url": "/static/css/main.050ab323.chunk.css"
+  },
+  {
+    "revision": "49ed86766552d0823811",
+    "url": "/static/js/2.1acfe672.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1acfe672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a297bb9ce13396a6e08",
+    "url": "/static/js/main.93d9b7a5.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "d64adc8a1d6a67800200d740ea6d8faa",
+    "url": "/static/media/404.d64adc8a.png"
+  }
+]);
