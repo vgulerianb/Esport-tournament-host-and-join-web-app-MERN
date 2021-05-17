263fb8a7167981fd3c53e19dd049bd3926d83382
Vikrant <vikrant@chalkstreet.com>
2021-05-17 18:56:30 +0530

    Game history schema

--- server/versions/v1/routor/game.router.js
+++ server/versions/v1/routor/game.router.js
@@ -1,10 +1,11 @@
 const { request } = require("express");
 const express = require("express");
 const router = express.Router();
-const { getGames, createGame, EditGame } = require("../services/game.service");
+const { getGames, createGame, EditGame, joinGame } = require("../services/game.service");
 const token = require("../../../utils/jwtVerifier")
 
 router.get("/", getGames);
+router.get("/join", joinGame);
 router.post("/", token.verifyToken, createGame);
 router.put("/", token.verifyToken, EditGame);
 
