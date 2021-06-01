152c49a8ef206694ba70e38a01e10369ac8a3cee
Vikrant <vikrant@chalkstreet.com>
2021-06-01 12:17:38 +0530

    Deploy

---
+++ client/build/precache-manifest.c285e2030322f1a0abb7eeccfdc93bd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3921a1a05f207b160dfb5207a1e2d4ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d81ea4dc0890803247bf",
+    "url": "/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "6f01bfa6f059d07e2cb8",
+    "url": "/static/css/main.050ab323.chunk.css"
+  },
+  {
+    "revision": "d81ea4dc0890803247bf",
+    "url": "/static/js/2.f457703d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.f457703d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f01bfa6f059d07e2cb8",
+    "url": "/static/js/main.f718f40b.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "d64adc8a1d6a67800200d740ea6d8faa",
+    "url": "/static/media/404.d64adc8a.png"
+  }
+]);
