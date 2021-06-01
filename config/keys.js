e60dc55c8752da13615b286efced7f7f2ec4be47
Vikrant <vikrant@chalkstreet.com>
2021-06-01 12:36:44 +0530

    deploy

--- config/keys.js
+++ config/keys.js
@@ -1,20 +1,20 @@
 module.exports = {
     mongodb: {
         dev: {
-            host: "cluster0-shard-00-00.6zxle.mongodb.net",
-            port: "27017",
-            database: "vg_esport",
-            username: "vg",
-            password: "j9C8t5yPF7pzq6D",
-            host_1: "cluster0-shard-00-00.6zxle.mongodb.net",
-            port_1: "27017",
-            host_2: "cluster0-shard-00-00.6zxle.mongodb.net",
-            port_2: "27017",
-            replicaSet: "atlas-3p41n1-shard-0"
+            host: process.env.mongo_host,
+            port: process.env.mongo_port,
+            database: process.env.mongo_database,
+            username: process.env.mongo_username,
+            password: process.env.mongo_password,
+            host_1: process.env.mongo_host,
+            port_1: process.env.mongo_port,
+            host_2: process.env.mongo_host,
+            port_2: process.env.mongo_port,
+            replicaSet: process.env.mongo_replicaSet
         },
     },
     mailgun: {
-        apiKey: "652b0085ab24da035dcee4de932f852f-c50a0e68-e13af7a4",
-        domain: "vikrantguleria.tech"
+        apiKey: process.env.mailgun_key,
+        domain: process.env.mailgun_domain
     }
 };
