32706fe2d8c978486dbdb0a1d732de192e8db772
Vikrant <vikrant@chalkstreet.com>
2021-06-05 14:35:11 +0530

    Mobile ui fixes

---
+++ client/build/precache-manifest.941a2fc7356ae482e8d57e2080fa9b7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3eb55c2ac4fb4c5c2d8e8630981357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2e4b9082df3e47b22d",
+    "url": "/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "b972d0657e9d1ba38e4c",
+    "url": "/static/css/main.5c6bf73e.chunk.css"
+  },
+  {
+    "revision": "3d2e4b9082df3e47b22d",
+    "url": "/static/js/2.c06db9da.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c06db9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b972d0657e9d1ba38e4c",
+    "url": "/static/js/main.7c1417e4.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "d64adc8a1d6a67800200d740ea6d8faa",
+    "url": "/static/media/404.d64adc8a.png"
+  }
+]);
